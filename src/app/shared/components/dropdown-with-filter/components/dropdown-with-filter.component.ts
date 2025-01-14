524622ff2bf5fdc7175c2e5b90a32ad52cb24e50
iizitounene <idir.izitounene@kubotal.io>
2025-01-14 16:42:03 +0000

    chore: shared component - add select component with search filter

---
+++ src/app/shared/components/dropdown-with-filter/components/dropdown-with-filter.component.ts
@@ -0,0 +1,70 @@
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Component, Input, Output, EventEmitter, HostListener, OnChanges, SimpleChanges } from '@angular/core';
+import { DropdownItem } from '../model/dropdown-item.model';
+
+@Component({
+  selector: 'app-dropdown-with-filter',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './dropdown-with-filter.component.html',
+  styleUrls: ['./dropdown-with-filter.component.scss'],
+})
+export class DropdownWithFilterComponent implements OnChanges {
+  @Input() orientation: 'top' | 'bottom' = 'bottom';
+  @Input() scrollable: boolean = true;
+  @Input() label: string = 'Filter ...';
+  @Input() noItemsLabel: string = 'No items found.';
+  @Input() options: DropdownItem[] = [];
+  @Output() selectionChange = new EventEmitter<DropdownItem>();
+
+  filteredOptions: DropdownItem[] = [];
+  filterText: string = '';
+  selectedOption: DropdownItem | null = null;
+  isDropdownOpen: boolean = false;
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['options'] && changes['options'].currentValue) {
+      this.filteredOptions = [...changes['options'].currentValue];
+      this.selectedOption = this.filteredOptions[0];
+    }
+  }
+
+  filterOptions() {
+    this.filteredOptions = this.options.filter(option =>
+      option.value.toLowerCase().includes(this.filterText.toLowerCase())
+    );
+  }
+
+  selectOption(option: DropdownItem) {
+    this.selectedOption = option;
+    this.selectionChange.emit(option);
+    this.isDropdownOpen = false;
+  }
+
+  toggleDropdown() {
+    this.isDropdownOpen = !this.isDropdownOpen;
+    if (this.isDropdownOpen) {
+      this.filterText = '';
+      this.filteredOptions = [...this.options];
+    }
+  }
+
+  @HostListener('document:click', ['$event'])
+  closeDropdownOnClick(event: Event) {
+    const targetElement = event.target as HTMLElement;
+
+    if (!targetElement.closest('.dropdown-filter-container')) {
+      this.isDropdownOpen = false;
+    }
+  }
+
+  @HostListener('document:mouseover', ['$event'])
+  closeDropdownOnMouseLeave(event: MouseEvent) {
+    const targetElement = event.target as HTMLElement;
+
+    if (!targetElement.closest('.dropdown-filter-container') && this.isDropdownOpen) {
+      this.isDropdownOpen = false;
+    }
+  }
+}
---
+++ src/app/shared/components/dropdown-with-filter/index.ts
@@ -0,0 +1,2 @@
+export * from './components/dropdown-with-filter.component';
+export * from './model/dropdown-item.model';
