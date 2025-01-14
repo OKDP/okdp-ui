1f97d870a6f8aecdae07adbbb209f1f351bb30e1
iizitounene <idir.izitounene@kubotal.io>
2025-01-14 14:51:12 +0000

    chore: initial commit - bootstrap Angular application
    Initialize project dependencies

---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  logging: {
+    enableConsole: true,
+    logLevel: 'debug',
+  },
+  debug: true,
+};
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  logging: {
+    enableConsole: true,
+    logLevel: 'info',
+  },
+  debug: false,
+};
