58e853250ec6897f038697000330b6c256a7c5b8
iizitounene <idir.izitounene@kubotal.io>
2025-07-07 11:14:05 +0200

    refactor: add common content header for pages content

---
+++ src/app/shared/components/content-header-title/services/title-bar.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TitleBarService {
+  private pageContentTitle = new BehaviorSubject<string>('home');
+  public pageContentTitle$ = this.pageContentTitle.asObservable();
+
+  TitleBarService() {}
+
+  setCurrentMenu(menu: string) {
+    sessionStorage.setItem('current-menu', menu);
+    this.pageContentTitle.next(menu);
+  }
+
+  setTitle(menu: string, title: string, icon: string, description: string) {
+    const currentSidebarMenuItem = { title, icon, description };
+    sessionStorage.setItem(menu, JSON.stringify(currentSidebarMenuItem));
+  }
+
+  getTitle(menu: string): { title: string; icon: string; description: string } {
+    return JSON.parse(sessionStorage.getItem(menu) || '{}');
+  }
+}
